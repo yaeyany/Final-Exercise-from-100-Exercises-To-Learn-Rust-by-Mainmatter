d36dbc291e517ecd2eca8d90b761f1d2e51d82ab
Yaeyany <philtbz@gmail.com>
2026-09-22 12:37:56 +0300

    Initialised

---
+++ README.md
@@ -0,0 +1,5 @@
+# Final Exercise from 100 Exercises To Learn Rust by Mainmatter
+
+## Description
+
+The final exercise in chapter 08 Futures: 8.8 Outro
---
+++ src/bin/axum_test.rs
@@ -0,0 +1,21 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // pass incoming GET requests on "/hello-world" to "hello_world" handler.
+    let app = Router::new().route("/hello-world", get(hello_world));
+
+    // write address like this to not make typos
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
